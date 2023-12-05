91b84ee31e2e082ad2df00d0f459da0dab8fc502
FreightDH <maxim.zolotkov@yandex.ru>
2023-12-05 20:05:00 +0300

    feat: add CustomInput component

---
+++ permitting-documentation/src/shared/components/CustomInput/CustomInput.jsx
@@ -0,0 +1,38 @@
+import cl from './CustomInput.module.scss';
+
+const CustomInput = ({
+  label,
+  type,
+  name,
+  required,
+  value,
+  onChange,
+  register,
+  errors,
+  validationSchema,
+}) => {
+  const inputClasses = [cl.input__input];
+  if (errors[name]) inputClasses.push(cl.err);
+
+  return (
+    <div className={cl.input}>
+      <label htmlFor={name} className={cl.input__label}>
+        {label}
+        {required && '*'}
+      </label>
+      <input
+        type={type}
+        id={name}
+        value={value}
+        className={inputClasses.join(' ')}
+        {...register(name, validationSchema)}
+        onChange={onChange}
+      />
+      {errors && errors[name] && (
+        <span className={cl.input__error}>{`${errors[name]?.message || 'Ошибка!'}`}</span>
+      )}
+    </div>
+  );
+};
+
+export default CustomInput;
